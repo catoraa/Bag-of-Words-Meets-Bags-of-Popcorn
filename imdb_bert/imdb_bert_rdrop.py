1ed67c8a3e18d50a5316cf3f26e7c20591db03a8
Catora <1164726499@qq.com>
2024-10-27 21:45:00 +0800

    1

--- imdb_bert/imdb_bert_rdrop.py
+++ imdb_bert/imdb_bert_rdrop.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     training_args = TrainingArguments(
         output_dir='./checkpoint',  # output directory
         num_train_epochs=3,  # total number of training epochs
-        per_device_train_batch_size=6,  # batch size per device during training
+        per_device_train_batch_size=4,  # batch size per device during training
         per_device_eval_batch_size=8,  # batch size for evaluation
         warmup_steps=500,  # number of warmup steps for learning rate scheduler
         weight_decay=0.01,  # strength of weight decay
