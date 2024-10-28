ce6be083bbf071d0ac6cdd75bd7517bb58ed6b3b
myh <1164726499@qq.com>
2024-10-28 20:45:39 +0800

    1

--- imdb_deberta/imdb_deberta_lora.py
+++ imdb_deberta/imdb_deberta_lora.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     lora_config = LoraConfig(
         r=16,
         lora_alpha=32,
-        # target_modules=['q_proj', 'v_proj'],
+        #target_modules=['q_proj', 'v_proj'],
         lora_dropout=0.05,
         bias="none",
         task_type=TaskType.SEQ_CLS
